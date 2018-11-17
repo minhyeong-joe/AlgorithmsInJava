c4dc564bad1d9f0919ef155e28f9f3c24997ce27
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-11-16 18:20:53 -0800

    initial commit with fibonacci sequence algorithm

---
+++ fibonacciSequence/src/fibonacciSequence/FibonacciSequence.java
@@ -0,0 +1,36 @@
+package fibonacciSequence;
+
+import java.util.Scanner;
+
+public class FibonacciSequence {
+	
+	public static void main(String[] args) {
+		
+		int n = 0;
+		Scanner input = new Scanner(System.in);
+		while (n >= 0) {
+			n = input.nextInt();
+			printFibonacci(n);			
+		}
+		input.close();
+		System.out.println("GOODBYE.");
+	}
+	
+	public static void printFibonacci(int n) {
+		for(int i = 1; i <= n; i++) {
+			System.out.print(fib(i) + " ");
+		}
+		System.out.println("");
+	}
+	
+	// return Nth fibonacci number.
+	public static int fib(int n) {
+		if(n == 1) {
+			return 1;
+		} else if(n == 0) {
+			return 0;
+		} else {
+			return fib(n-2) + fib(n-1);
+		}
+	}
+}
