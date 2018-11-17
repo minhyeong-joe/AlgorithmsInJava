9522ec9761b816671bbd97b6b51af85a6c0a4e70
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-11-17 14:14:37 -0800

    primeFactorization implemented

--- primeFactorization/src/primeFactorization/primeFactorization.java
+++ primeFactorization/src/primeFactorization/primeFactorization.java
@@ -1,9 +1,38 @@
 package primeFactorization;
 
+import java.util.Scanner;
+
 public class primeFactorization {
+	
+	void factor(int n) {
+		if(n%2 == 0) {
+			System.out.print("2 ");
+			factor(n/2);
+		} else {
+			for(int i = 3; i <= n; i += 2) {
+				if(n%i == 0) {
+					System.out.print(i + " ");
+					factor(n/i);
+					break;
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n = 0;
+		Scanner input = new Scanner(System.in);
+		primeFactorization prime = new primeFactorization();
+		
+		while(n >= 0) {
+			n = input.nextInt();
+			
+			prime.factor(n);
+			System.out.println();
+		}
+		
+		System.out.println("GOODBYE");
+		input.close();
 
 	}
 
