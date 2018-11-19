56665fffbf14ea8f80a42217e644d55274265e8d
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-11-18 18:00:53 -0800

    added: Palindrome check

---
+++ src/PalindromeCheck.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+	
+	public boolean isPalindrome(String word) {
+		int left = 0;
+		int right = word.length()-1;
+		while(right > left) {
+			if(word.charAt(left) != word.charAt(right)) {
+				return false;
+			} else {
+				left++;
+				right--;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String userInput = input.next();
+		
+		PalindromeCheck checker = new PalindromeCheck();
+		if (checker.isPalindrome(userInput))
+			System.out.printf("'%s' is a palindrome.\n", userInput);
+		else
+			System.out.printf("'%s' is not a palindrome.\n", userInput);
+		
+		input.close();
+		
+	}
+
+}
